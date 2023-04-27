6f9e16813898878294f4aff9fe0e0e4ab2b23b4d
aat <aat8001@gmail.com>
2023-04-27 22:34:55 +0530

    promise.any

--- JS/MyPromises.js
+++ JS/MyPromises.js
@@ -41,15 +41,31 @@
 // })
 
 
+//allSettled
+// const promise2= Promise.allSettled([
 
-const promise2= Promise.allSettled([
+//     new Promise(res=>setTimeout(() => { res('resolved 1') }, 1000)),
+//     new Promise((res,rej)=>setTimeout(() => { rej('resolved 2') }, 2000)),
+//     new Promise(res=>setTimeout(() => { res('resolved 3') }, 4000))
+// ])
+
+// console.log(promise2)
+// promise2.then(res=>{
+//     console.log(res, 'sssss')
+// })
+
+
+//race
+const promise3= Promise.race([
 
     new Promise(res=>setTimeout(() => { res('resolved 1') }, 1000)),
     new Promise((res,rej)=>setTimeout(() => { rej('resolved 2') }, 2000)),
     new Promise(res=>setTimeout(() => { res('resolved 3') }, 4000))
 ])
 
-console.log(promise2)
-promise2.then(res=>{
+console.log(promise3)
+promise3.then(res=>{
     console.log(res, 'sssss')
 })
+
+// any
