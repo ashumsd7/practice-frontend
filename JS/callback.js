20b8ddee36d4aa433c02cd1f7fedc46837dea566
aat <aat8001@gmail.com>
2023-04-27 13:06:42 +0530

    callback hell

---
+++ JS/callback.js
@@ -0,0 +1,20 @@
+// console.log("namaste");
+
+// setTimeout(function () {
+//   console.log("js");
+// }, 5000);
+
+// console.log("s2");
+
+const cart = ["shoes", "kurta", "pants"];
+
+api.createOrder(cart, function () {
+  //create order & call payment
+  api.proceedToPayment(function () {
+    // do payment and then print order summary
+    api.showOrderSummary(function () {
+        //print order summary & updtae the waller
+      api.updateThWallet();
+    });
+  });
+});
